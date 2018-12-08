20b9ddc27470f0d3411834b622aeb9bab5ab8b1a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-08 10:13:00 +0600

    video rent service

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,7 +7,7 @@ namespace Vidly.Models
 {
     public class Movie
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
     }
 }
