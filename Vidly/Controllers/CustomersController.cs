75a0050d31cc491bce40add2f56709db9dbaca66
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-10 19:19:28 +0600

    Section 4 - add form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -36,6 +36,25 @@ namespace Vidly.Controllers
             return View("CustomerForm", viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         // GET: Customers / Index
 
         public ViewResult Index()
