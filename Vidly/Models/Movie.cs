6ec1609392cad9af5b8254bd5cb7934d54212ba6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-10 08:36:02 +0600

    section 3 database

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -13,5 +13,11 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
     }
 }
