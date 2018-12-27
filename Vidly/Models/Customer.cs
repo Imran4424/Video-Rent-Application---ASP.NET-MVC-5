75b9280ff338e1aa9d486935f44b4a4c41354460
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 19:03:32 +0600

    video rental application

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -23,7 +23,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
-        [Required]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
