01006458f910d69c867617d2ffad22342644d7ae
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 19:12:54 +0600

    video rental application

--- Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vidly.Models
 {
-    public class Min18YearsIfAMember
+    public class Min18YearsIfAMember : ValidationAttribute
     {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            
+            if(customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
 
+            if(customer.Birthdate == null)
+            {
+                return new ValidationResult("Birthdate is required");
+            }
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+
+            if (age >= 18)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Customer should be at least 18 years old");
+            }
+
+        }
     }
 }
