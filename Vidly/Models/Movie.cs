af7cd53a01a34198ae807d0b3bf055cd5b86ed1d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-27 17:38:30 +0600

    video rental application

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -23,9 +23,11 @@ namespace Vidly.Models
         public DateTime DateAdded { get; set; }
 
         [Display(Name = "Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
+        [Required]
         public byte NumberInStock { get; set; }
 
 
