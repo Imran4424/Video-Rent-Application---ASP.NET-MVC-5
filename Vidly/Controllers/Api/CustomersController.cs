dffd52dfa7106c6122613e7fe2aa9f0552767724
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-28 10:12:55 +0600

    video rental application - web api

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -21,7 +21,35 @@ namespace Vidly.Controllers.Api
 
         public IEnumerable<Customer> GetCustomers()
         {
-
+            return _context.Customers.ToList();
         }
+
+        // GET /api/customers/1
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c >= c.Id == id);
+
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            {
+
+            }
+        }
+        
     }
 }
