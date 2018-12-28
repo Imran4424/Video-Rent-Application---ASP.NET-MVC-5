939c1752af972e69724f833a3be63b63608fc735
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-28 08:08:56 +0600

    video rental application - peer view model

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -11,33 +11,33 @@ namespace Vidly.ViewModels
     {
         public IEnumerable<Genre> Genres { get; set; }
 
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
+        
 
         [Display(Name = "Genre")]
         [Required]
-        public byte GenreId { get; set; }
+        public byte? GenreId { get; set; }
 
-        public DateTime DateAdded { get; set; }
+        
 
         [Display(Name = "Release Date")]
         [Required]
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
         [Required]
-        public byte NumberInStock { get; set; }
+        public byte? NumberInStock { get; set; }
 
         public string Title
         {
             get
             {
-                if (Movie != null && Movie.Id != 0)
+                if (Id != 0)
                     return "Edit Movie";
 
                 return "New Movie";
