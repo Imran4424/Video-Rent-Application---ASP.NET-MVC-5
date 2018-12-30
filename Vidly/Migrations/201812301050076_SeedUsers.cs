4a93aaf64c7ed21ee9df150a846b3c22705e9600
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-30 16:59:45 +0600

    authentication

--- Vidly/Migrations/201812301050076_SeedUsers.cs
+++ Vidly/Migrations/201812301050076_SeedUsers.cs
@@ -7,6 +7,11 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
+            Sql(@"
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'0523ac03-a5a1-4cff-a9f6-d8aaaddceca9', N'guest@vidly.com', 0, N'AMIM3kkffuTIMWFWJ6d0Hh83WY3+axOj3uNl4l2HaEizyBDvMl4sLYNyG8iK65Kr/A==', N'198b7a7a-87ed-4d91-9379-a58139b496c9', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'6629e918-c16d-4496-bc00-7ac075044ada', N'admin@vidly.com', 0, N'ADZObTey41Dn4UyYAq6F6N44nDs+AEAkGKzLu+FhBP5478zcoy0IQNpL5BFHN9Z+Dg==', N'6ae6174a-bdd0-4be8-a635-1dbc2333359d', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+
+            ");
         }
         
         public override void Down()
